4751782a39554497bdf8772a593bfc6af905557b
michaelgermini <michael@germini.info>
2016-05-28 23:56:50 +0200

    Node server

---
+++ server.js
@@ -0,0 +1,12 @@
+
+    var http = require('http');
+     
+    var s = http.createServer(function(request, response) {
+        response.writeHead(200);
+        response.write("Hello World");
+        response.end();
+    });
+     
+    s.listen(8082);
+     
+    console.log("Listening on http://127.0.0.1:8082/");
